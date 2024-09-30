1b513c52ae910ff12bdc318c373bc5a9e1c8e9b4
duypv92 <33708096+duypv92@users.noreply.github.com>
2024-09-30 16:57:29 +0700

    Add files via upload

--- common.js
+++ common.js
@@ -185,7 +185,7 @@ const getFuturesBalance = async (asset) => {
     try {
         const balances = await client.futuresAccountBalance();
         const balanceInfo = balances.find(b => b.asset === asset);
-        return balanceInfo ? balanceInfo.balance : null;
+        return balanceInfo ? balanceInfo.availableBalance : null;
     } catch (error) {
         console.error('Error fetching futures balance:', error);
         return null;
@@ -471,7 +471,7 @@ const confirmMarketStatus = async (symbol) => {
         }
         utils.customLog(`lastShortTermMA: ${lastShortTermMA}, lastLongTermMA: ${lastLongTermMA} (Short>Long => BUY(${utils.FgYellow}${lastShortTermMA > lastLongTermMA}${utils.Reset}), SELL(${utils.FgYellow}${lastShortTermMA < lastLongTermMA}${utils.Reset}))`);
         utils.customLog(`lastRSI: ${lastRSI}, (> 50 => BUY(${utils.FgYellow}${lastRSI > 50}${utils.Reset}), SELL(${utils.FgYellow}${lastRSI < 50 && lastRSI > 30}${utils.Reset}))`);
-        utils.customLog(`lastest volume: ${latestVolume},averageVolume: ${averageVolume} (lastest > averageVolume(${utils.FgYellow}${latestVolume > averageVolume}${utils.Reset}))`);
+        utils.customLog(`lastest volume: ${latestVolume},averageVolume: ${averageVolume} (lastest > averageVolume(${utils.FgYellow}${latestVolume > (averageVolume * 0.75)}${utils.Reset}))`);
         utils.customLog(`→　New suggest action: ${utils.FgYellow}${action}${utils.Reset}`);
         return { action, stopLoss, takeProfit, lastRSI };
     } catch (error) {
